96813e209ac64f603eeaa9bcf0462c885bd30688
cbrown <cbrown@decisionpatterns.com>
2011-05-22 16:58:27 -0700

    - R/optigrab.r: Exploring alternative method

--- R/optigrab.r
+++ R/optigrab.r
@@ -3,20 +3,33 @@
 #  R --args --date 2011-05-17 -b --end-date=2011-05-20  -a 
 #  Using Rscript: commandArgs reports
 #    /opt/r/R-2.13.0-default/lib/R/bin/exec/R --slave --no-restore --file=./test.r --args --args --date 2011-05-17 -b --end-date=2011-05-20 -a 
-
+#  We should take the
+#  Features:
+#  API: 
+#  ( opt=NULL, fun=NULL, env=NULL, message=NULL, required=FALSE ) 
+#  - optigrab( 'reps', as.integer) 
+#  --date x y z --> date <- c(x,y,z)
+#  -a --date x --date y --date z o
+#  IF --data 2011-10-10 -a THEN a will be interpreted as a logical
+#  IF --date 2011-10-01
 
 # make.names
 
-optigrab <- function( opt=NULL, fun=NULL, env=NULL, message=NULL, required=FALSE ) {
+optigrab <- 
+function( opt=NULL, fun=NULL, env=NULL, message=NULL, required=FALSE ) {
 
     args <- commandArgs() 
 
   # STRIP '--args' AND BEFORE
+  #  - Generally we are not interested in arguments before (the first)
+  #    '--args' 
     wh.args <- grep( "--args", args )[1] 
     args <- args[ (wh.args+1):length(args) ] 
 
 
   # EXPAND/Split  '=' 
+  #  - Options defined with and '=', such as '-a=5' or '--alpha=5'
+  #    are split into '-a' '5' and '--alpha' '5', respectively.
     wh.eq <- grep( "=", args )    
     for( i in wh.eq ) {
       name.val <- strsplit( args[i], "=" )[[1]]     
@@ -24,9 +37,11 @@ optigrab <- function( opt=NULL, fun=NULL, env=NULL, message=NULL, required=FALSE
       args <- append( args, name.val[2], wh.eq ) 
     }
  
-  # 
+
+  # Identify the position (wh.opt) and names (nms) of the options
+  # in the command arguments
     wh.opt <- grep( "^-", args ) 
-    nms <- sub( "^-+", "", args )[wh.opt]
+    nms    <- sub( "^-+", "", args )[wh.opt]
    
  
   # GIVEN: opt 
@@ -71,5 +86,14 @@ optigrab <- function( opt=NULL, fun=NULL, env=NULL, message=NULL, required=FALSE
 
 }
 
+ll <- list()
+for( i in 1:length(args) ) {
 
-
+  if( grepl('^-', args[[i]] ) ) {
+    nm <- sub( "^-+", "", args[i] )
+    cat(nm)
+    while( grepl( '^-') && i <= length(args) ) {
+      ll[[nm]] <- if( is.null(ll[[nm]]) )   
+    }
+  }
+}
