5609610ceb090517efceebabc1515c57eed830e4
Christopher Brown <cbrown@decisionpatterns.com>
2015-04-15 14:49:41 -0700

    OSR-67: Implement opt_help

--- R/opt_help.R
+++ R/opt_help.R
@@ -4,20 +4,22 @@
 
 # opt_help <- function( args=commandArgs() ) {
 
-#' Provide program usage information and exit.  
+#' Automatic usage/help information
 #' 
-#' Prints program usage information and exits. Usage information comes from 
-#' the \code{help} arguments to \code{opt_grab}. Calls to 
-#' \code{opt_grab} following the \code{opt_help} call will not be considered, so
-#' it is best to specify make all \code{opt_grab} calls before \code{opt_help}.
+#' Prints program usage information and, if used non-interactively, exits. 
 #' 
 #' @param opts character; vector from which to parse options 
 #'        (default: \code{commandArgs()} )
 #' 
-#' Usage information from \code{opt_grab} calls made after \code{optihelp()} is 
-#' used will will not be shown.  It is considered best practice to handle all 
+#' Usage information is generated from \code{opt_get} calls made prior to 
+#' \code{opt_help}. \code{opt_help} shused will will not be shown.  It is considered best practice to handle all 
 #' option parsing in a block at the beginning of the application. \code{optihelp}
 #' would be best placed at the end of that block 
+#' 
+#' @return 
+#'   logical; returns \code{TRUE} if command-line options contain a help flag,
+#'   \code{FALSE} otherwise. Mainly \code{opt_help} is used for side-effects of 
+#'   printing usage/help information, 
 #' 
 #' @seealso 
 #'   \code{\link[base]{commandArgs}}
@@ -29,32 +31,37 @@
 #' 
 #' @export
 
-opt_help <- function( opts=commandArgs() ) {
+opt_help <- function( flags=c('help','?'), opts=commandArgs() ) {
+  
+  patterns <- getOption('optigrab')$style$name_to_flag(flags)
   
   script.path <- opt_get_path(opts=opts)
   script.name <- NULL
   
   opts <- opt_expand(opts)
-  if(! any( grepl( "--help$|-\\?$|-h$", opts ) ) ) {
-    return(FALSE)
-  }
+  
+  if( ! any(patterns %in% opts) ) return(FALSE)
+  
+  #if(! any( grepl( "--help$|-\\?$", opts ) ) ) {
+  #  return(FALSE)
+  # }
 
-  if (! is.na(script.path)) {
-    script.name <- basename(script.path)
-  }
+  if( ! is.na(script.path) ) script.name <- basename(script.path)
+
   
   command <- opt_get_command(opts=opts)
-  if (is.na(command)) {
-    command <- NULL
-  }
-  
+  if( is.na(command) ) command <- NULL
+
+
   opts <- getOption( "optigrab" )$help
 
   # Construct help message
-  cat( paste("Help for", script.name, command, sep=" "), sep="\n" )
+    cat( paste("Usage: ", script.name, command, sep=" "), sep="\n" )
 
-  for( nm in names(opts) ) 
-    cat(paste(nm, ":", opts[[nm]], sep=" "), sep="\n")
+    for( nm in names(opts) ) 
+      cat( paste(' ', nm, ":", opts[[nm]], sep=" "), sep="\n" )
+  
+  if( ! interactive() ) quit( save = FALSE )
   
   return(TRUE) 
   
